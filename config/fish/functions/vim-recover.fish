a5e40336efa1fc422cdf44f3b002a706bddd83aa
David Emett <dave@sp4m.net>
2017-05-30 17:20:15 +0100

    fish: vim-recover function

---
+++ config/fish/functions/vim-recover.fish
@@ -0,0 +1,10 @@
+function vim-recover
+    set -l swp_glob '*.sw[mnop]'
+
+    find . -name $swp_glob -exec vim -r {} -c q \;
+
+    read -l yn -p 'echo \'Delete swap files? \'' -n 1
+    if test $yn = y
+        find . -name $swp_glob -delete
+    end
+end
