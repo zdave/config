5b9d13a11f0f54c900dd46cd9b48cfec6dab031f
David Emett <dave@sp4m.net>
2020-12-15 12:11:31 +0000

    fish: Explicitly use bright colors
    Don't assume terminal will automatically make bold colors bright.

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -7,25 +7,25 @@ end
 if status is-interactive
     set fish_greeting
 
-    set fish_color_error -o red
-    set fish_color_command -o white
-    set fish_color_end -o cyan
+    set fish_color_error -o brred
+    set fish_color_command -o brwhite
+    set fish_color_end -o brcyan
     set fish_color_param normal
     set fish_color_comment brblack
     set fish_color_search_match --background=brblack
-    set fish_color_operator -o yellow
-    set fish_color_escape -o magenta
+    set fish_color_operator -o bryellow
+    set fish_color_escape -o brmagenta
     set fish_color_quote normal
     set fish_color_redirection green
     set fish_color_autosuggestion brblack
     set fish_color_selection --background=brblack
     set fish_color_valid_path --underline
 
-    set fish_pager_color_prefix -o white
+    set fish_pager_color_prefix -o brwhite
     set fish_pager_color_completion normal
     set fish_pager_color_description brblack
     set fish_pager_color_progress normal
 
-    set fish_color_host -o blue
-    set fish_color_cwd -o yellow
+    set fish_color_host -o brblue
+    set fish_color_cwd -o bryellow
 end
--- config/fish/functions/fish_prompt.fish
+++ config/fish/functions/fish_prompt.fish
@@ -1,18 +1,18 @@
 function fish_prompt
     set -l s $status
     if test $s -ne 0
-        set_color -o red
+        set_color -o brred
         printf '%s ' $s
     end
 
-    set_color -o black
+    set_color -o brblack
     printf '%s ' (date '+%H:%M')
 
     set_color $fish_color_host
     printf '%s ' $hostname
 
     if test $SHLVL -gt 1
-        set_color -o cyan
+        set_color -o brcyan
         printf '%s ' $SHLVL
     end
 
@@ -21,7 +21,7 @@ function fish_prompt
 
     set -l git_sha (git rev-parse --short HEAD ^/dev/null)
     if test $status -eq 0
-        set_color -o green
+        set_color -o brgreen
         set -l git_branch (git symbolic-ref --short HEAD ^/dev/null)
         if test $status -eq 0
             printf ' %s' $git_branch
@@ -30,7 +30,7 @@ function fish_prompt
         end
     end
 
-    set_color -o black
+    set_color -o brblack
     printf ' %s' (jobs -c | tail -n+1)
 
     set_color normal
